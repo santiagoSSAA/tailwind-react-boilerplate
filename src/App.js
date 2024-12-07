a167d0e6b1e4531bfc4f3dea28168a2250bd70c1
Santiago <santiago.sanchez.ssaa@gmail.com>
2024-12-07 10:24:19 -0500

    changes about new landing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useEffect, useState} from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -6,7 +6,20 @@ import Testimonials from './components/Testimonials';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 
+import { createClient } from "@supabase/supabase-js";
+
+const apiKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const projectUrl = process.env.REACT_APP_SUPABASE_PROJECT_HOST;
+const supabase = createClient(projectUrl,apiKey);
+
 const App = () => {
+  getCountries();
+  
+  async function getCountries(){
+    const {data} = await supabase.from("countries").select();
+    console.log(data); 
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
